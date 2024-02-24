cd3b9ab911c4f32408dd8b7cccfd912854c64e9a
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-02-23 23:07:04 -0500

    Empezando el taller de bucles repetitivos

--- BuclesRepetitivos/src/buclesrepetitivos/BuclesRepetitivos.java
+++ BuclesRepetitivos/src/buclesrepetitivos/BuclesRepetitivos.java
@@ -6,12 +6,19 @@ import java.util.Scanner;
  * @author JulianAndresGuzman
  */
 
-/
+/*Una heladería desea obtener un reporte semanal de ventas. Para ello debe capturar en un
+sistema todas las facturas de ventas realizadas. Cada factura contiene la cantidad y el sabor de
+las paletas vendidas. Los sabores y los precios son: limón ($2500), nuez ($2800), lulo ($2600),
+fresa ($2000) y crema ($3000). El sistema debe imprimir la cantidad de paletas vendidas de cada
+sabor, el valor vendido y la venta total. Considere que una factura puede tener varias paletas de
+diferentes sabores. */
+
 public class BuclesRepetitivos {
 
     public static void main(String[] args) {
         
-        System.out.println("Escribe un número y te mostraré su tabla del 1 hasta el 10");
+        String[] sabores = {"limón", "nuez", "lulo", "fresa", "crema"};
+        int [] precios = {2500, 2800, 2600, 2000, 3000};
         
 
     }
