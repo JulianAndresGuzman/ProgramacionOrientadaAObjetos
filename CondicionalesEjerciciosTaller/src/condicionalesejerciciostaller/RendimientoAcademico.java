064024db81a3bc46c21937ad47dc9c3b436b93ca
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-02-24 12:18:12 -0500

    En proceso, está complicada la situación

--- CondicionalesEjerciciosTaller/src/condicionalesejerciciostaller/RendimientoAcademico.java
+++ CondicionalesEjerciciosTaller/src/condicionalesejerciciostaller/RendimientoAcademico.java
@@ -35,20 +35,32 @@ public class RendimientoAcademico {
         //variables:
         Scanner in = new Scanner(System.in);
         String tecnologia = "tecnologia";
-        String Prefesional = "profesional";
+        String profesional = "profesional";
         float promedio;
 
         //Código:
         System.out.println("Cual es tu tipo de alumno? (usa minusculas)");
-        System.out.println("*tecnologia   *profesional)");
-
+        System.out.println("*tecnologia   *profesional");
         String tipoAlumno = in.nextLine();
+        
 
-        System.out.println("Escribe el promedio del alumno: ");
-
+        //Valor de precio de la tecnología 18.000 por cada 5 créditos 
         if (tipoAlumno.equals(tecnologia)) {
-
-        }
+            System.out.println("Es tecnologo");
+            System.out.println("Escribe el promedio del alumno: ");
+            promedio = in.nextFloat();
+            if (promedio >= 9.5){
+                System.out.println("Tiene un buen promedio, puede cursar hasta 55 creditos con un descuento del 50%");
+                System.out.println("Escribe los números de créditos que desea cursar: ");
+                int creditos = in.nextInt();
+                System.out.println("El valor total de la matricula es: " + (((18000 * (creditos/5))) - (18000 * (creditos/5)) * 0.5));
+            }
+            
+        } else if (tipoAlumno.equals(profesional)){
+        System.out.println("Eres profesional");
+        System.out.println("Escribe el promedio del alumno: ");
+        } else {
+        System.out.println("Asegurate de haber elegido una de las opciones, asegurate de respetar minusculas y mayusculas");}
 
     }
 }
