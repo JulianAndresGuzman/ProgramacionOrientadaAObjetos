36528d00eb16552c2975766c730b61c0c3fd49e6
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-02-26 18:16:05 -0500

    ObreroSalario Terminado

--- CondicionalesEjerciciosTaller/src/condicionalesejerciciostaller/ObreroSalario.java
+++ CondicionalesEjerciciosTaller/src/condicionalesejerciciostaller/ObreroSalario.java
@@ -9,21 +9,29 @@ aumento del 20% por cada hora extra.
  * @author JulianAndres
  */
 package condicionalesejerciciostaller;
+
 import java.util.Scanner;
+
 public class ObreroSalario {
 
     public static void main(String args[]) {
-        
+
         //variables
         Scanner in = new Scanner(System.in);
         int horasTrabajadas;
-        
+        int horasExtras;
+
         //Pregunta de horas trabajadas.
         System.out.println("Cuantas horas trabajó semanalmente?");
         horasTrabajadas = in.nextInt();
-        
-        if (horasTrabajadas <= 40){
-            System.out.println("paga semanal:  " + (horasTrabajadas * 5000));
+
+        if (horasTrabajadas <= 40) {
+            System.out.println("Salario: " + (horasTrabajadas * 5000));
+        } else {
+            double salario = 40 * 5000; // Salario base por las primeras 40 horas
+            horasExtras = horasTrabajadas - 40;
+            salario += horasExtras * (5000 * 1.20); // 20% de aumento por cada hora extra
+            System.out.println("Salario: " + salario);
         }
     }
 }
