a06b319efdf529b9f616ad436b6622e700872216
Julian Andres Guzman Cuellar <julianguzmn28@gmail.com>
2024-03-06 12:43:36 -0500

    Programa funcionando?

--- BuclesRepetitivos/src/buclesrepetitivos/penitenciario.java
+++ BuclesRepetitivos/src/buclesrepetitivos/penitenciario.java
@@ -19,21 +19,30 @@ public class penitenciario {
 
         System.out.println("Ingrese el numero de los prisioneros");
         int numeroPrisioneros = in.nextInt();
-        
+        in.nextLine();
+
         String nombres[] = new String[numeroPrisioneros];
         int condena[] = new int[nombres.length];
 
         for (int n = 0; n < numeroPrisioneros; n++) {
-            
+
             System.out.println("Ingrese nombre del prisionero");
             nombres[n] = in.nextLine();
-            
-            System.out.println("ingrese condena del prisionero en meses: " + nombres[n]);
+
+            System.out.println("ingrese condena del prisionero en meses de: " + nombres[n]);
             condena[n] = in.nextInt();
+            in.nextLine();
         }
 
         for (int n = 0; n < numeroPrisioneros; n++) {
-            System.out.println("la condena de " + nombres[n] + " Es de: " + condena[n] + " meses");
+
+            if (condena[n] < 36) {
+                System.out.println("(**condena de menos de 36 meses**");
+                System.out.println("la condena de " + nombres[n] + " Es de: " + condena[n] + " meses");
+            } else {
+                System.out.println("(**condena de mas de 36 meses**)");
+                System.out.println("la condena de " + nombres[n] + " Es de: " + condena[n] + " meses");
+            }
 
         }
     }
